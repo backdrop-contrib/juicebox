b69f9b569ae319dea3c54be3dfae7633e4f10e76
Ryan Jacobs <rjacobs@422459.no-reply.drupal.org>
2013-01-01 22:23:28 -0600

    Initial commit - establish base files, hook implementations, etc.

---
+++ themes/juicebox-view.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file juicebox-view.tpl.php
+ */
+?>
+<!--START JUICEBOX EMBED-->
+<script src="<?php print $juicebox_path ?>/juicebox.js"></script>
+<script>
+  new juicebox({
+    configUrl : '<?php print $config_url_path ?>',
+    containerId : 'juicebox-container',
+    galleryWidth : '<?php print $style_options['width'] ?>',
+    galleryHeight : '<?php print $style_options['height'] ?>'
+  });
+
+</script>
+<div id="juicebox-container"></div>
+<!--END JUICEBOX EMBED-->
