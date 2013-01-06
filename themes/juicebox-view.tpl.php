2916c24382d3978122c488ebaebfcd4e2f665a4d
Ryan Jacobs <rjacobs@422459.no-reply.drupal.org>
2013-01-06 12:20:19 -0600

    Add some sanitizing to XML outputs and run cocer checks.

--- themes/juicebox-view.tpl.php
+++ themes/juicebox-view.tpl.php
@@ -9,8 +9,8 @@
   new juicebox({
     configUrl : '<?php print $config_url_path ?>',
     containerId : 'juicebox-container',
-    galleryWidth : '<?php print $style_options['width'] ?>',
-    galleryHeight : '<?php print $style_options['height'] ?>'
+    galleryWidth : '<?php print check_plain($style_options['width']) ?>',
+    galleryHeight : '<?php print check_plain($style_options['height']) ?>'
   });
 </script>
 <div id="juicebox-container"></div>
